bb0761e4690d3a296753cc862caade35c8ed2c09
yuliia_harashchenko <yharashchenko@ukr.net>
2024-04-12 00:03:26 +0300

    add use strict

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function checkForSpam(message) {
   const lowerCaseMessage = message.toLowerCase();
   return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
